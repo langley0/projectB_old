71431ba577baae2d01b010a065237b458fe28cb2
helloraina <helloraina@nexon.co.kr>
2019-03-21 17:10:15 +0900

    캐릭터 정보창, 스테이지정보 화면 추가

--- src/main.js
+++ src/main.js
@@ -27,8 +27,41 @@ function initApp() {
     game.keyDown(e.keyCode);
   },false);
 
+  // index.html 내 ui 테스트용 코드입니다. - 해림
+  $('#menu-modal').click(function (){
+    var modal = document.getElementById('modalContainer');
+    modal.style.display = 'block';
+    var closeBtn = document.getElementById('button-goBack');
+    var inventoryBtn = document.getElementById('button-inventory');
+
+    closeBtn.addEventListener('click', function(event){
+      modal.style.display = "none";
+    });
+
+    inventoryBtn.addEventListener('click', function() {
+      modal.style.display = "none";
+      var inventory = document.getElementById('inventory');
+      inventory.style.display="block";
+
+      inventory.addEventListener('click', function(event){
+        inventory.style.display = "none";
+      });
+    });
+  });
+
+  // $('#game-start').click(function (){
+  //   var intro = document.getElementById('intro');
+  //   intro.style.display = 'none';
+  //   app.start("TEST");
+  // });
+
   // 플레이 버튼을 누르면 시작하게 해야 하지만... 일단은 그냥하자
   app.start("TEST");
+
+  var navi = document.getElementById('navi');
+  navi.style.opacity = 1;
+  var stage = document.getElementById('stageInfo');
+  stage.style.display = 'block';
 }
 
 initApp();
